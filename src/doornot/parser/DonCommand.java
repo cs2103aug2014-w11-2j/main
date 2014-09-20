4bd17cec6f3d5cdd8d460ce1e6967bca6cbc92ba
HarithaRamesh <haritha_1211@hotmail.com>
2014-09-20 18:25:55 +0800

    Added parser package and created DonParser and DonCommand Interfaces
    DonParser added unfinished
    DonCommand added unfinished

---
+++ src/doornot/parser/DonCommand.java
@@ -0,0 +1,15 @@
+package doornot.parser;
+
+public class DonCommand implements IDonCommand{
+
+	public DonCommand() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public CommandType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
