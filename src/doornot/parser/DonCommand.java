ab3c90219285cd923145735d478c122b3569a47b
HarithaRamesh <haritha_1211@hotmail.com>
2014-09-21 11:10:07 +0800

    Added the parameter getMethods in DonCommand
    added specific types in CommandType
    getID
    getName
    getDeadline
    getNewName
    getNewStartDate
    getNewEndDate
    getNewDeadline

--- src/doornot/parser/DonCommand.java
+++ src/doornot/parser/DonCommand.java
@@ -1,5 +1,7 @@
 package doornot.parser;
 
+import java.util.Calendar;
+
 public class DonCommand implements IDonCommand{
 
 	public DonCommand() {
@@ -8,6 +10,48 @@ public class DonCommand implements IDonCommand{
 
 	@Override
 	public CommandType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Calendar getDeadline() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNewName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Calendar getNewStartDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Calendar getNewEndDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Calendar getNewDeadline() {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/doornot/parser/IDonCommand.java
+++ src/doornot/parser/IDonCommand.java
@@ -1,10 +1,14 @@
 package doornot.parser;
 
+import java.util.Calendar;
+
 public interface IDonCommand {
 	
 	
 	public enum CommandType {
-		ADD, EDIT, DELETE, SEARCH, MARK, UNDO, INVALID 
+		ADD_FLOAT, ADD_TASK, ADD_EVENT, EDIT_ID_NAME, EDIT_ID_DATE, EDIT_ID_EVENT,
+		EDIT_NAME, EDIT_DATE, EDIT_EVENT, DELETE_ID, DELETE, SEARCH_NAME, SEARCH_DATE, SEARCH_ID,
+		MARK_ID, MARK, UNDO, INVALID 
 	}
 	
 	/**
@@ -13,5 +17,56 @@ public interface IDonCommand {
 	 * @return the type of command
 	 */
 	public CommandType getType();
+	
+	/**
+	 * Gets the ID of the task
+	 * @return int ID
+	 */
+	public int getID();
+	
+	/**
+	 * Gets the name of the task being edited/searched/marked
+	 * @return name of edited task
+	 */
+	public String getName();
+	
+	/**
+	 * Gets the date of task being edited/searched/marked
+	 * @return
+	 */
+	public Calendar getDeadline();
+	
+	
+	/**
+	 * Gets the new name of the task
+	 * Used for add, edit commands
+	 * 
+	 * @return new name
+	 */
+	public String getNewName();
+	
+	/**
+	 * Gets the new start date of the event task
+	 * Used for add, edit commands
+	 *
+	 * @return new start date
+	 */
+	public Calendar getNewStartDate();
+	
+	/**
+	 * Gets the new end date of the event task
+	 * Used for add, edit commands
+	 * 
+	 * @return new end date
+	 */
+	public Calendar getNewEndDate();
+	
+	/**
+	 * Gets the new deadline of the task
+	 * Used for add, edit commands
+	 * 
+	 * @return new deadline
+	 */
+	public Calendar getNewDeadline();
 	
 }
