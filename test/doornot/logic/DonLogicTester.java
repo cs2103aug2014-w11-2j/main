015f8bb98493e7ba7d8ca41f39f261d3911117bb
nopol10 <bobnewbie@gmail.com>
2014-09-21 11:59:39 +0800

    Added tests for the undo functionality.

--- test/doornot/logic/DonLogicTester.java
+++ test/doornot/logic/DonLogicTester.java
@@ -13,19 +13,22 @@ public class DonLogicTester {
 		DonLogic logic = new DonLogic();
 		IDonResponse response = logic.runCommand("add Finish homework");
 		IDonTask task = response.getTasks().get(0);
-		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS, response.getResponseType());
+		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS,
+				response.getResponseType());
 		assertEquals("Finish homework", task.getTitle());
 		assertEquals(null, task.getStartDate());
 		assertEquals(null, task.getEndDate());
 		assertEquals(IDonTask.TaskType.FLOATING, task.getType());
 	}
-	
+
 	@Test
 	public void testAddDeadlineTask() {
 		DonLogic logic = new DonLogic();
-		IDonResponse response = logic.runCommand("add Finish homework 21122014_2030");
+		IDonResponse response = logic
+				.runCommand("add Finish homework 21122014_2030");
 		IDonTask task = response.getTasks().get(0);
-		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS, response.getResponseType());
+		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS,
+				response.getResponseType());
 		assertEquals("Finish homework", task.getTitle());
 		Calendar deadline = task.getStartDate();
 		assertEquals(2014, deadline.get(Calendar.YEAR));
@@ -36,13 +39,15 @@ public class DonLogicTester {
 		assertEquals(null, task.getEndDate());
 		assertEquals(IDonTask.TaskType.DEADLINE, task.getType());
 	}
-	
+
 	@Test
 	public void testAddDurationTask() {
 		DonLogic logic = new DonLogic();
-		IDonResponse response = logic.runCommand("add Finish homework 21122014_2030 21122014_2356");
+		IDonResponse response = logic
+				.runCommand("add Finish homework 21122014_2030 21122014_2356");
 		IDonTask task = response.getTasks().get(0);
-		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS, response.getResponseType());
+		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS,
+				response.getResponseType());
 		assertEquals("Finish homework", task.getTitle());
 		Calendar startDate = task.getStartDate();
 		Calendar endDate = task.getEndDate();
@@ -51,7 +56,7 @@ public class DonLogicTester {
 		assertEquals(21, startDate.get(Calendar.DAY_OF_MONTH));
 		assertEquals(20, startDate.get(Calendar.HOUR_OF_DAY));
 		assertEquals(30, startDate.get(Calendar.MINUTE));
-		
+
 		assertEquals(2014, endDate.get(Calendar.YEAR));
 		assertEquals(Calendar.DECEMBER, endDate.get(Calendar.MONTH));
 		assertEquals(21, endDate.get(Calendar.DAY_OF_MONTH));
@@ -59,26 +64,27 @@ public class DonLogicTester {
 		assertEquals(56, endDate.get(Calendar.MINUTE));
 		assertEquals(IDonTask.TaskType.DURATION, task.getType());
 	}
-	
+
 	/*
 	 * Deletion tests
-	 * 
 	 */
-	
+
 	@Test
 	public void testDeleteTaskWithID() {
 		DonLogic logic = new DonLogic();
 		IDonResponse addResponse = logic.runCommand("add Finish homework");
-		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS, addResponse.getResponseType());
+		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS,
+				addResponse.getResponseType());
 		int taskID = addResponse.getTasks().get(0).getID();
-		IDonResponse delResponse = logic.runCommand("del "+taskID);
-		assertEquals(IDonResponse.ResponseType.DEL_SUCCESS, delResponse.getResponseType());
+		IDonResponse delResponse = logic.runCommand("del " + taskID);
+		assertEquals(IDonResponse.ResponseType.DEL_SUCCESS,
+				delResponse.getResponseType());
 		IDonTask deletedTask = delResponse.getTasks().get(0);
 		assertEquals(taskID, deletedTask.getID());
 		assertEquals("Finish homework", deletedTask.getTitle());
 
 	}
-	
+
 	/*
 	 * Editing tests
 	 */
@@ -86,21 +92,28 @@ public class DonLogicTester {
 	public void testEditTitleWithID() {
 		DonLogic logic = new DonLogic();
 		IDonResponse addResponse = logic.runCommand("add Finish homework");
-		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS, addResponse.getResponseType());
+		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS,
+				addResponse.getResponseType());
 		IDonTask changedTask = addResponse.getTasks().get(0);
-		IDonResponse editResponse = logic.runCommand("edit "+changedTask.getID()+" Ignore homework");
-		assertEquals(IDonResponse.ResponseType.EDIT_SUCCESS, editResponse.getResponseType());
+		IDonResponse editResponse = logic.runCommand("edit "
+				+ changedTask.getID() + " Ignore homework");
+		assertEquals(IDonResponse.ResponseType.EDIT_SUCCESS,
+				editResponse.getResponseType());
 		assertEquals("Ignore homework", changedTask.getTitle());
 	}
-	
+
 	@Test
 	public void testEditDeadlineWithID() {
 		DonLogic logic = new DonLogic();
-		IDonResponse addResponse = logic.runCommand("add Finish homework 10052014_0810");
-		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS, addResponse.getResponseType());
+		IDonResponse addResponse = logic
+				.runCommand("add Finish homework 10052014_0810");
+		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS,
+				addResponse.getResponseType());
 		IDonTask changedTask = addResponse.getTasks().get(0);
-		IDonResponse editResponse = logic.runCommand("edit "+changedTask.getID()+" 21052014_1020");
-		assertEquals(IDonResponse.ResponseType.EDIT_SUCCESS, editResponse.getResponseType());
+		IDonResponse editResponse = logic.runCommand("edit "
+				+ changedTask.getID() + " 21052014_1020");
+		assertEquals(IDonResponse.ResponseType.EDIT_SUCCESS,
+				editResponse.getResponseType());
 		Calendar date = changedTask.getStartDate();
 		assertEquals(21, date.get(Calendar.DAY_OF_MONTH));
 		assertEquals(Calendar.MAY, date.get(Calendar.MONTH));
@@ -108,15 +121,19 @@ public class DonLogicTester {
 		assertEquals(10, date.get(Calendar.HOUR_OF_DAY));
 		assertEquals(20, date.get(Calendar.MINUTE));
 	}
-	
+
 	@Test
 	public void testEditDatesWithID() {
 		DonLogic logic = new DonLogic();
-		IDonResponse addResponse = logic.runCommand("add Finish homework 10052014_0810 10052014_0850");
-		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS, addResponse.getResponseType());
+		IDonResponse addResponse = logic
+				.runCommand("add Finish homework 10052014_0810 10052014_0850");
+		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS,
+				addResponse.getResponseType());
 		IDonTask changedTask = addResponse.getTasks().get(0);
-		IDonResponse editResponse = logic.runCommand("edit "+changedTask.getID()+" 21052014_1020 21052014_1140");
-		assertEquals(IDonResponse.ResponseType.EDIT_SUCCESS, editResponse.getResponseType());
+		IDonResponse editResponse = logic.runCommand("edit "
+				+ changedTask.getID() + " 21052014_1020 21052014_1140");
+		assertEquals(IDonResponse.ResponseType.EDIT_SUCCESS,
+				editResponse.getResponseType());
 		Calendar startDate = changedTask.getStartDate();
 		Calendar endDate = changedTask.getEndDate();
 		assertEquals(21, startDate.get(Calendar.DAY_OF_MONTH));
@@ -124,7 +141,7 @@ public class DonLogicTester {
 		assertEquals(2014, startDate.get(Calendar.YEAR));
 		assertEquals(10, startDate.get(Calendar.HOUR_OF_DAY));
 		assertEquals(20, startDate.get(Calendar.MINUTE));
-		
+
 		assertEquals(21, endDate.get(Calendar.DAY_OF_MONTH));
 		assertEquals(Calendar.MAY, endDate.get(Calendar.MONTH));
 		assertEquals(2014, endDate.get(Calendar.YEAR));
@@ -132,4 +149,82 @@ public class DonLogicTester {
 		assertEquals(40, endDate.get(Calendar.MINUTE));
 	}
 
+	/*
+	 * Undo tests
+	 */
+
+	@Test
+	public void testUndoAfterAdd() {
+		DonLogic logic = new DonLogic();
+		IDonResponse addResponse = logic.runCommand("add Finish homework");
+		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS,
+				addResponse.getResponseType());
+		int taskID = addResponse.getTasks().get(0).getID();
+
+		IDonResponse undoResponse = logic.runCommand("undo");
+		assertEquals(IDonResponse.ResponseType.UNDO_SUCCESS,
+				undoResponse.getResponseType());
+
+		IDonResponse searchResponse = logic.runCommand("s " + taskID);
+		assertEquals(IDonResponse.ResponseType.SEARCH_EMPTY,
+				searchResponse.getResponseType());
+
+	}
+
+	@Test
+	public void testUndoAfterDelete() {
+		DonLogic logic = new DonLogic();
+		// Add
+		IDonResponse addResponse = logic.runCommand("add Finish homework");
+		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS,
+				addResponse.getResponseType());
+		int taskID = addResponse.getTasks().get(0).getID();
+		// Delete
+		IDonResponse delResponse = logic.runCommand("del " + taskID);
+		assertEquals(IDonResponse.ResponseType.DEL_SUCCESS,
+				delResponse.getResponseType());
+		IDonTask deletedTask = delResponse.getTasks().get(0);
+		// Run a search which should return empty
+		IDonResponse searchResponse = logic.runCommand("s " + taskID);
+		assertEquals(IDonResponse.ResponseType.SEARCH_EMPTY,
+				searchResponse.getResponseType());
+		// Undo the deletion
+		IDonResponse undoResponse = logic.runCommand("undo");
+		assertEquals(IDonResponse.ResponseType.UNDO_SUCCESS,
+				undoResponse.getResponseType());
+
+		IDonResponse searchResponse2 = logic.runCommand("s " + taskID);
+		assertEquals(IDonResponse.ResponseType.SEARCH_SUCCESS,
+				searchResponse2.getResponseType());
+		IDonTask readdedTask = searchResponse2.getTasks().get(0);
+		assertEquals(readdedTask, deletedTask);
+	}
+
+	@Test
+	public void testUndoAfterEdit() {
+		DonLogic logic = new DonLogic();
+		// Add
+		IDonResponse addResponse = logic.runCommand("add Finish homework");
+		assertEquals(IDonResponse.ResponseType.ADD_SUCCESS,
+				addResponse.getResponseType());
+		IDonTask beforeEditTask = addResponse.getTasks().get(0).clone();
+		int taskID = addResponse.getTasks().get(0).getID();
+		// Edit
+		IDonResponse editResponse = logic.runCommand("edit " + taskID
+				+ " Do work");
+		assertEquals(IDonResponse.ResponseType.EDIT_SUCCESS,
+				editResponse.getResponseType());
+
+		// Undo the edit
+		IDonResponse undoResponse = logic.runCommand("undo");
+		assertEquals(IDonResponse.ResponseType.UNDO_SUCCESS,
+				undoResponse.getResponseType());
+
+		IDonResponse searchResponse = logic.runCommand("s " + taskID);
+		assertEquals(IDonResponse.ResponseType.SEARCH_SUCCESS,
+				searchResponse.getResponseType());
+		IDonTask uneditedTask = searchResponse.getTasks().get(0);
+		assertEquals(uneditedTask, beforeEditTask);
+
+	}
 }
