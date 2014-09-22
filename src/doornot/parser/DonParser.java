55350f14ca9098162c4c5bff06ad9cee34cf52c1
HarithaRamesh <haritha_1211@hotmail.com>
2014-09-22 22:35:19 +0800

    Updated parseCommand method in DonParser

--- src/doornot/parser/DonParser.java
+++ src/doornot/parser/DonParser.java
@@ -1,16 +1,37 @@
 package doornot.parser;
 
+import doornot.parser.IDonCommand.CommandType;
+
 public class DonParser implements IDonParser{
 
 	public DonParser() {
 		// TODO Auto-generated constructor stub
 	}
-
+	private String userCommand;
+	private CommandType dType;
+	private DonCommand dCommand;
+	
 	@Override
 	public DonCommand parseCommand(String command) {
 		
-//		readFirstWord(command);
-//		DonCommand.CommandType hi = DonCommand.CommandType.ADD_EVENT;
+		dCommand = new DonCommand();
+		userCommand = command;
+		dType = getCommandType();
+		setDonCommand();
+		return dCommand;
+		
 	}
+	
+	private CommandType getCommandType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private void setDonCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
 }
